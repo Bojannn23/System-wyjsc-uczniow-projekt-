4a8fe6cfca730b66d30830350d2e1c8f3f1ae3f9
Bojan <oskarbojanowski23@gmail.com>
2026-09-10 12:12:56 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,23 @@
-import './App.css'
-import '../node_modules/bootstrap'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginScreen from './LoginScreen.jsx';
 
-function Header({children}) {
-  return(
-    <header>{children}</header>
-  )
-}
-  
-
-function Btn({children}) {
-  return(
-    <button>{children}</button>
-  )
-}
-
-function Card({children}) {
-  return(
-    <div style={{
-      width: "400px",
-      // height: "400px",
-      border: "1px solid #ccc",
-      borderRadius: "8px",
-      boxShadow: "0 5px 7px gray",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      padding: "30px"
-    }}>{children}</div>
-  )
-}
-
+// Przykładowy komponent widoku po zalogowaniu
+const Dashboard = () => (
+  <div className="container mt-5">
+    <h2>Witaj w panelu głównym!</h2>
+  </div>
+);
 
 function App() {
-  return(
-    <> 
-      <Header>
-        <h1>Projekt WEB 2.0</h1>
-      </Header>
-      <Btn>
-        skibidi
-      </Btn>
-      <main>
-        <Card>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti sapiente necessitatibus recusandae in sequi laborum mollitia nesciunt quam similique doloremque. Totam voluptates voluptatum ad ea tempora eveniet dolorem ipsam exercitationem!
-        </Card>
-        <Card>
-          <Btn>
-            Mama mia
-          </Btn>
-        </Card>
-      </main>
-    </>
-  )
-  
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginScreen />} />
+        <Route path="/panel" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
